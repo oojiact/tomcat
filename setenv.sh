51e107e4ac30afe3de30cd42815b255abe920ed4
oojiact <33948432+oojiact@users.noreply.github.com>
2019-03-08 16:57:31 +0700

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,6 @@
+export CATALINA_OPTS="$CATALINA_OPTS -Xmx1024m"
+export CATALINA_OPTS="$CATALINA_OPTS -Dorg.apache.catalina.STRICT_SERVLET_COMPLIANCE=false"
+export CATALINA_OPTS="$CATALINA_OPTS -Dorg.apache.catalina.connector.RECYCLE_FACADES=true"
+export CATALINA_OPTS="$CATALINA_OPTS -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=false"
+export CATALINA_OPTS="$CATALINA_OPTS -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=false"
+export CATALINA_OPTS="$CATALINA_OPTS -Dorg.apache.coyote.USE_CUSTOM_STATUS_MSG_IN_HEADER=false"
